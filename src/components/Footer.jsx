d096618f75223f274e216b5e538869b95795d4e8
Kishan Kumar <Kishankumar8166@gmail.com>
2024-02-11 23:53:06 +0530

    intial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,14 @@ const Footer = () => {
   return <footer>
     <div>
         <h1>Made with 💖by Kishan</h1>
-        <p></p>
+        <p>@all rights reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.instagram.com/_kishn_/" target={"blank"}>"Instagram"</a>
+        </div>
     </div>
     </footer>
 }
